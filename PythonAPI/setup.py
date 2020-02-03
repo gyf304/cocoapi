0e66e36b8eeff19417412dca5fc21049c1a96327
Yifan Gu <gyf304@users.noreply.github.com>
2020-02-03 18:18:34 -0500

    Update setup.py

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, Extension
-import numpy as np
+
+INCLUDE_DIRS = ['../common']
+
+try:
+    import numpy as np
+    INCLUDE_DIRS.insert(0, np.get_include())
+except ImportError:
+    pass
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
@@ -8,7 +15,7 @@ ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        include_dirs = INCLUDE_DIRS,
         extra_compile_args=['-std=c99'],
     )
 ]
@@ -20,7 +27,8 @@ setup(
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+        'matplotlib>=2.1.0',
+        'numpy>=1.18.1',
     ],
     version='2.0',
     ext_modules= ext_modules
